8ee68b0e4e54a0225e5d95de2e1e02eed7519fc8
Oleh <trubatur@ukr.net>
2019-04-08 12:32:07 +0000

    error404

---
+++ js/index.js
@@ -0,0 +1,21 @@
+document.querySelector('.get-html-ajax')
+    .addEventListener('click', getHtmlAjax);
+    
+    
+    
+const READY_STATE_FINISHED = 4;
+const HTTP_STATUS_CODE_OK = 200;
+
+
+function getHtmlAjax() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState === READY_STATE_FINISHED
+    && xhr.status == HTTP_STATUS_CODE_OK) {
+        document.querySelector('.html-placeholder')
+        .innerHTML = xhr.responseText;
+    }
+    xhr.open('GET' , 'client-data.html', true);
+    xhr.send();
+}
+}
