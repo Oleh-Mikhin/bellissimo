eae356373c3d6d22232daffeb8a078c5fe651efd
Oleh <trubatur@ukr.net>
2019-04-10 13:33:28 +0000

    pre release

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 document.querySelector('.get-html-ajax')
     .addEventListener('click', getHtmlAjax);
-    
-    
-    
+
+
+
 const READY_STATE_FINISHED = 4;
 const HTTP_STATUS_CODE_OK = 200;
 
@@ -10,13 +10,13 @@ const HTTP_STATUS_CODE_OK = 200;
 function getHtmlAjax() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-    if (xhr.readyState === READY_STATE_FINISHED
-    && xhr.status == HTTP_STATUS_CODE_OK) {
-        document.querySelector('.html-placeholder')
-        .innerHTML = xhr.responseText;
+        if (xhr.readyState === READY_STATE_FINISHED &&
+            xhr.status == HTTP_STATUS_CODE_OK) {
+            document.querySelector('.html-placeholder')
+                .innerHTML = xhr.responseText;
+        }
     }
-  } 
-  xhr.open('GET' , 'client-data.html', true);
+    xhr.open('GET', 'client-data.html', true);
     xhr.send();
 
 }
